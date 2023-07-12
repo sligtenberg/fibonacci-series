f5d56049a848ed0264c352f51d0b4aceefa7930b
stephen ligtenberg <bountyhunter24@gmail.com>
2023-07-12 17:54:39 -0400

    done

--- ruby/fibonacci.rb
+++ ruby/fibonacci.rb
@@ -1,18 +1,22 @@
+# RECURSIVE SOLUTION:
+
 def fibonacci(num)
-  # type your code in here
-  if num < 2 
-    return num
-  else
-    i = 0
-    last_two = [0, 1]
-    until i == num do
-      sum = last_two[0] + last_two[1]
-      last_two = [last_two[1], sum]
-      i += 1
-    end
-    return last_two[0]
-  end
+  return num if num < 2
+  return fibonacci(num-1) + fibonacci(num-2)
 end
+
+# NON-RECURSIVE SOLUTION:
+# def fibonacci(num)
+#   return num if num < 2
+#   i = 0
+#   last_two = [0, 1]
+#   until i == num do
+#     sum = last_two[0] + last_two[1]
+#     last_two = [last_two[1], sum]
+#     i += 1
+#   end
+#   return last_two[0]
+# end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 0"
